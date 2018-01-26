0263f922d1c90897cd363e1ac610efd2607c70e2
Will Bradley <bradley@ficonsulting.com>
2018-01-26 16:07:11 -0500

    Updates to format NA values and more unit tests.

--- R/format_acct.R
+++ R/format_acct.R
@@ -4,7 +4,8 @@
 #'
 #' @param x numeric vector
 #' @param digits numeric to denote number of decimal places
-#' @param shown.in character to express value in thousands, millions or billions
+#' @param shown.in character to express value in thousands, millions or billions.
+#' Accepts \code{c('k', 'm', 'b')}.
 #'
 #' @examples
 #'
@@ -12,13 +13,19 @@
 #'
 #' format_acct(100, digits = 0)
 #'
-#' mtcars %>% mutate(msrp = runif(nrow(.), 20000, 30000), msrp_format = format_acct(msrp))
+#' library(dplyr)
+#' mtcars %>% mutate(msrp = runif(nrow(.), 20000, 30000), msrp_format = format_acct(msrp, shown.in = 'k'))
 #'
 #' }
 #'
+#' @return character vector of \code{length(x)} with desired formatting
 #' @export
 
 format_acct <- function(x, digits = 2, shown.in = NULL) {
+
+  if (!is.numeric(x)) stop('x must be numeric!')
+
+  # If shown.in is not null then divide by appropriate value
 
   if (!is.null(shown.in)) {
 
@@ -34,15 +41,23 @@ format_acct <- function(x, digits = 2, shown.in = NULL) {
 
   }
 
+  # Round x to specified number of decimal places
   format_x <- format(round(abs(x), digits), nsmall = digits, big.mark = ',')
+  # Remove any whitespace
   format_x <- gsub('\\s+', '', format_x)
 
-  negative <- x < 0
+  negative <- !is.na(x) & x < 0
 
+  # Add dollar sign and ensure that round numbers (i.e. $100) still show desired decimal places
+  # Negative values will have parens
   val <- paste0('$',
                 ifelse(negative, '(', ''),
                 format_x, shown.in,
                 ifelse(negative, ')', ''))
+
+  # Set NA values to NA
+  is_na <- is.na(x)
+  val[is_na] <- NA
 
   return(val)
 
--- tests/testthat/test_format_acct.R
+++ tests/testthat/test_format_acct.R
@@ -4,7 +4,10 @@ x <- c(1000, -1000, NA)
 
 test_that("format_acct returns expected output", {
 
-  expect_true(identical(format_acct(x)[1], "$1,000.00"))
+  expect_error(format_acct('test'))
   expect_true(any(is.na(format_acct(x))))
+  expect_true(identical(format_acct(x[1]), "$1,000.00"))
+  expect_true(identical(format_acct(x[2]), "$(1,000.00)"))
+  expect_true(identical(format_acct(x[1], shown.in = 'k'), "$1.00K"))
 
 })
